414fddd9ca22b0771564f11960fe1637dffa7bf7
Q3rkses <cyprian.github@gmail.com>
2024-02-19 16:03:38 +0100

    added some button and door functionality

---
+++ source/driver/door.h
@@ -0,0 +1,32 @@
+/**
+ * @file door.h
+ * @brief This file contains the door functionality.
+*/
+
+#pragma once
+#include <stdbool.h>
+
+/**
+ * @brief This struct contains the door state.
+*/
+typedef struct Door{
+    bool isOpen;
+} Door;
+
+/**
+ * @brief This function turns on the door lamp
+ * @return Door
+*/
+void Door_Open(Door door);
+
+/**
+ * @brief This function turns off the door lamp
+ * @return Door
+*/
+void Door_Close(Door door);
+
+/**
+ * @brief This function returns the door state
+ * @return bool
+*/
+bool Get_Door_State(Door door);
