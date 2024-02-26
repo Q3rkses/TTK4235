eb2cce412c09ce517283ef81d13cb62603be7849
Q3rkses <cyprian.github@gmail.com>
2024-02-26 14:32:25 +0100

    added elevatorpanel matrix

--- source/driver/buttonhandler.c
+++ source/driver/buttonhandler.c
@@ -1,5 +1,13 @@
 #include "buttonhandler.h"
 
+Elevatorpanel_init(Elevatorpanel panel){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            panel.PanelButtonState[i][j] = 0;
+        }
+    }
+    printf("Elevator panel initialized\n");
+}
 
 bool Get_Stop_Button_State(Buttonhandler buttonhandler){
     if (elevio_stopButton == 0){
@@ -12,8 +20,12 @@ bool Get_Stop_Button_State(Buttonhandler buttonhandler){
     return buttonhandler.StopBtnState;
 }
 
-void Turn_On_Stop_Button_Lamp(Buttonhandler buttonhandler){
+void Turn_On_Stop_Button_Lamp(){
     elevio_setStopLamp(1);
+}
+
+void Turn_Off_Stop_Button_Lamp(){
+    elevio_setStopLamp(0);
 }
 
 bool Get_Obstruction_Button_State(Buttonhandler buttonhandler){
